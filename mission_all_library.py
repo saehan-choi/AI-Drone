e63fcc0d6ff4258a270bbfadb8f251fabbaa1f3c
sean <gmlssns5859@naver.com>
2021-08-21 04:59:40 +0900

    library

--- mission_all_library.py
+++ mission_all_library.py
@@ -1,21 +1,13 @@
 from time import sleep
 from e_drone.drone import *
 from e_drone.protocol import *
-# import matplotlib.pyplot as plt
 import math
-
 
 drone = Drone()
 drone.open()
 
 class library:    
-
-    def landing(self):
-        print("미션완료 착륙합니다.")
-        drone.sendLanding()
-        sleep(0.01)
-        drone.close()
-
+    
     def eventAltitude(self,altitude):
         global height
         print(f'현재고도:{altitude.rangeHeight}m')
@@ -26,100 +18,112 @@ class library:
         drone.setEventHandler(DataType.Altitude, self.eventAltitude)
         drone.sendRequest(DeviceType.Drone, DataType.Altitude)
         sleep(0.1)
-        # 짧으면 짧을수록 좋음. 시간 많이 X
 
-    def stright_flying(self,positionX, m_s, delay_time):
+    def same_flying(self,mission3=False):
+        if mission3:
+            print("이륙")
+            drone.sendTakeOff()
+            sleep(3)
+            drone.sendControlWhile(0, 0, 0, 0, 1000)
+            sleep(1)
+        else:
+            print("이륙")
+            drone.sendTakeOff()
+            sleep(3)
+            print("호버링 3초")
+            drone.sendControlWhile(0, 0, 0, 0, 3000)
+            sleep(3)
+            self.stright_flying(0.8,0.4,3)
+            self.altitude_control(1.5,0.4,4)
 
-        drone.sendControlPosition(positionX, 0, 0, m_s, 0,0)
-        print(f"전방 {positionX}m, {m_s}m/s 속도로 이동합니다.")
+    def stright_flying(self,distance, m_s, delay_time, right_left=False, sleepON=False):
+        if right_left:
+            drone.sendControlPosition(0, distance, 0, m_s, 0,0)
+            print(f"측방 {distance}m, {m_s}m/s 속도로 이동합니다.")
+        else:
+            drone.sendControlPosition(distance, 0, 0, m_s, 0,0)
+            print(f"전방 {distance}m, {m_s}m/s 속도로 이동합니다.")
         sleep(delay_time)
+        if sleepON == True:
+            print("5초간 정지비행합니다.")
+            sleep(5)
 
-
-    def altitude_control(self,Hope_Altitude, m_s, delay_time):
-        # 단위 : m/s
-        self.handler()  # 고도 차이 알아내기
+    def altitude_control(self,Hope_Altitude, m_s, delay_time, sleepoff=False):
+        self.handler()
         altitude = []
-        # 다시 실행되면 altitude를 []로 리셋하기때무에 [0]값을 받아도 상관x
         altitude.append(height)
-
         diffrence = Hope_Altitude-altitude[0]
         print(f'{Hope_Altitude}m까지 {diffrence}m 차이가 나므로 고도 조종합니다.')
-        
         drone.sendControlPosition(0, 0, diffrence, m_s, 0, 0)
         sleep(delay_time)
-        self.handler() # 고도 확인작업
-        # Hope_Altitude -> 원하는고도 (m단위)
-        # append로 한 이유는 변수가 자꾸 튕겨나가서
-        print("고도조정완료 5초간 정지비행 합니다.")
-        sleep(5)
+        self.handler()
+        if sleepoff==True:
+            pass
+        else:
+            print("고도조정완료 5초간 정지비행 합니다.")
+            sleep(5)
 
-
-    def square_flying(self,positionX, m_s,delay_time):    
-        # 정사각형 회전 
+    def square_flying(self,distance, m_s,delay_time):    
         for i in range(4):
-            print(f"go front start_{i+1}회")
-            drone.sendControlPosition(positionX,0,0,m_s,0,0)
+            print(f"정사각형비행_전진_{i+1}회")
+            drone.sendControlPosition(distance,0,0,m_s,0,0)
             sleep(delay_time)
-            ###### 아직 0.5m이동으로 해놓았음 테스트하려고 ㅎㅎ...
-            # 대회 제출떈 이동거리 1m로 바꾸고 0.5m/s로 바꿀것.
-
-            # 좌로 90도 rotate
-            print(f"rotate start_{i+1}회")
+            print(f"정사각형비행_회전__{i+1}회")
             drone.sendControlPosition(0,0,0,0,90,30)
             sleep(4)
-            # 3초만에 돌고 1초 대기.
         print("정사각형 비행완료 5초간 정지비행 합니다.")
         sleep(5)
-        # 정지비행 5초 포함
 
-    def circle_flying(self, radius ,m_s, delay_time, eight_shape_rotate=False):
+    def circle_flying(self, radius ,m_s, delay_time, eight_shape_rotate=False, u_turn=False):
         x = [] 
         y = [] 
-        
-        # 지름이 1이라했으니 0.5 로 고쳐야함
+        x_sampling=[]
+        y_sampling=[]
+
         if eight_shape_rotate:
-            for theta in range(0, 180):
-                x.append(radius * math.cos(math.radians(theta)))   
+            for theta in range(0, 360):
+                x.append(-radius * math.cos(math.radians(theta)))   
                 y.append(radius * math.sin(math.radians(theta)))         
-
-            for theta in range(0, 360):
+            for theta in range(0, 361):
                 x.append(-2*radius + radius * math.cos(math.radians(theta)))   
-                y.append(-radius * math.sin(math.radians(theta))) 
-
-            for theta in range(180, 360):
-                x.append(radius * math.cos(math.radians(theta)))   
-                y.append(radius * math.sin(math.radians(theta))) 
-
-            for theta in range(0, 720):
-                print(f'x좌표:{x[theta]}')
-                print(f'y좌표:{y[theta]}')
-                drone.sendControlPosition(x[theta],y[theta],0,m_s,0,0)
-                sleep(delay_time)
-            # plt.scatter(x, y)
-            # plt.show()
+                y.append(radius * math.sin(math.radians(theta)))         
+            for theta in range(0, 721):
+                if theta == 0:
+                    pass
+                elif theta % 10 == 0:
+                    drone.sendControlPosition(x[theta]-x[theta-10],y[theta]-y[theta-10],0,m_s,0,0)
+                    sleep(delay_time)
             print("8자 비행완료 5초간 정지비행 합니다.")
+            sleep(5)
 
-        else:
-            for theta in range(0, 360):
-                # 0도에서 360도까지 1도(degree) 간격
-                x.append(radius * math.cos(math.radians(theta)))   
+        elif u_turn:
+            for theta in range(0,181):
+                x.append(radius - radius * math.cos(math.radians(theta)))   
                 y.append(radius * math.sin(math.radians(theta))) 
-
-                print(f'x좌표:{x[theta]}')
-                print(f'y좌표:{y[theta]}')
-                drone.sendControlPosition(x[theta],y[theta],0,m_s,0,0)
-                sleep(delay_time)
+            for theta in range(0,181):
+                if theta == 0:
+                    pass
+                elif theta % 10 == 0:
+                    drone.sendControlPosition(y[theta]-y[theta-10],x[theta]-x[theta-10],0,m_s,0,0)
+                    sleep(delay_time)
+            
+        else:
+            for theta in range(0, 361):
+                x.append(radius - radius * math.cos(math.radians(theta)))   
+                y.append(radius * math.sin(math.radians(theta))) 
+            for theta in range(0,361):
+                if theta == 0:
+                    pass
+                elif theta % 10 ==0:
+                    drone.sendControlPosition(y[theta]-y[theta-10],x[theta]-x[theta-10],0,m_s,0,0)
+                    sleep(delay_time)
             print("원 비행완료 5초간 정지비행 합니다.")
-        sleep(5)
-        # 정지비행 5초 포함
+            sleep(5)
 
     def zigzag_flying(self, distance, m_s, angle, delay_time):
-        
         angle = angle / 2
-
         positionX = distance * math.sin(math.radians(angle))
         positionY = distance * math.cos(math.radians(angle))
-
         for i in range(2):
             drone.sendControlPosition(positionX, positionY, 0, m_s, 0, 0)
             sleep(delay_time)
@@ -142,19 +146,9 @@ class library:
             sleep(delay_time)
         print("지그재그 비행완료 5초간 정지비행합니다.")    
         sleep(5)
-    # 정지비행 5s
 
-
-    def same_flying(self):
-        print("이륙")
-        drone.sendTakeOff()
-        sleep(3)   # waiting for takeoff
-
-        print("호버링 3초")
-        drone.sendControlWhile(0, 0, 0, 0, 3000)
-        sleep(3)
-        self.stright_flying(0.8,0.4,3)
-
-        self.altitude_control(1.5,0.4,4)
-
-
+    def landing(self):
+        print("미션완료 착륙합니다.")
+        drone.sendLanding()
+        sleep(0.1)
+        drone.close()    
